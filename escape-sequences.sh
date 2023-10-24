d69396d7d345bfd5209277d79f5a98bb66ad8875
Antti Kivi <antti.kivi@visiosto.fi>
2023-10-24 14:57:40 +0300

    feat: add the utility for escape sequences

---
+++ escape-sequences.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+trap 'echo "Error in $0 at line $LINENO"; exit 1' ERR
+
+ESC="\033"
+export ESC_RESET="$ESC[0m"
+export ESC_BOLD="$ESC[1m"
+export ESC_DIM="$ESC[2m"
+export ESC_UNDERLINE="$ESC[4m"
+
+export ESC_LIGHT_RED="$ESC[91m"
+export ESC_LIGHT_GREEN="$ESC[92m"
+export ESC_LIGHT_CYAN="$ESC[96m"
+
+echo -e "${ESC_DIM}Setting the escape sequence variables.$ESC_RESET"
