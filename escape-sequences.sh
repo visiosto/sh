a126bb4a8ae85996bc800942b1acce2351d18c39
Antti Kivi <antti.kivi@visiosto.fi>
2023-10-27 14:08:31 +0300

    feat: add more colours

--- escape-sequences.sh
+++ escape-sequences.sh
@@ -8,6 +8,12 @@ export ESC_BOLD="$ESC[1m"
 export ESC_DIM="$ESC[2m"
 export ESC_UNDERLINE="$ESC[4m"
 
+export ESC_DEFAULT_COLOR="$ESC[39m"
+
+export ESC_RED="$ESC[31m"
+export ESC_GREEN="$ESC[32m"
+export ESC_CYAN="$ESC[36m"
+
 export ESC_LIGHT_RED="$ESC[91m"
 export ESC_LIGHT_GREEN="$ESC[92m"
 export ESC_LIGHT_CYAN="$ESC[96m"
